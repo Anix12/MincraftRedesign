3e55056e8235c07cc9aa42305e2694a3638d3b5e
Anix12 <adhavaniket1221@gmail.com>
2024-12-29 19:03:19 +0530

    first commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -37,7 +37,7 @@ const Loader = () => {
       >
         <div className="w-full flex flex-col h-full gap-3">
           <img
-            src="https://ik.imagekit.io/talib/minecraft%20assets%20/gallery/full%20mogang.svg?updatedAt=1719747041882"
+            src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
             className="md:block hidden w-full h-full"
             alt=""
           />
